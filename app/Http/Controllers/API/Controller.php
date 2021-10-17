2ac1abee3d0a6db27c09b68d020eda9c0dae5fda
volkv <pvolkv@gmail.com>
2021-10-17 12:30:59 +0300

    wip

--- app/Http/Controllers/API/Controller.php
+++ app/Http/Controllers/API/Controller.php
@@ -10,24 +10,24 @@ class Controller extends BaseController
     public function getRecomendations($userID)
     {
 
-        $books = \DB::select("select b.id, b.title, count(*) cnt, prcnt from user_book_histories final inner join books b on b.id = final.book_id inner join (
+        $books = \DB::select("select b.is_book_jsn, b.id, b.title, cnt, prcnt from user_book_histories final inner join book_uniques b on b.id = final.book_id inner join (
 
-select ubh.user_id,
+    select ubh.user_id,
 
-        (count(*)   /   (select count(*) from user_book_histories where user_id = ubh.user_id)::float)  prcnt
-from user_book_histories h
-         inner join user_book_histories ubh on ubh.book_id = h.book_id
+           count(*) cnt,    (count(*)   /   (select count(*) from user_book_histories where user_id = ubh.user_id)::float)  prcnt
+    from user_book_histories h
+             inner join user_book_histories ubh on ubh.book_id = h.book_id
 
-where h.user_id = $userID
+    where h.user_id = $userID
 
-group by ubh.user_id
-having ubh.user_id != $userID and (count(*) / (select count(*) from user_book_histories where user_id = ubh.user_id)::float) != 1
-order by prcnt desc
-limit 20) best on best.user_id = final.user_id
+    group by ubh.user_id
+    having ubh.user_id != $userID and (count(*) / (select count(*) from user_book_histories where user_id = ubh.user_id)::float) != 1
+    order by prcnt desc
+    limit 20) best on best.user_id = final.user_id
 
 where book_id not in (select book_id from user_book_histories where user_id = $userID)
-group by b.id, b.title, prcnt
-order by cnt desc, prcnt desc");
+group by b.id, b.title, cnt, prcnt
+order by prcnt desc ,cnt desc");
 
 
         $json = [];
