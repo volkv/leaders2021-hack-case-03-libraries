5a41f41e25c3508295b3a5f89e3995605e71e9c9
volkv <pvolkv@gmail.com>
2021-10-24 19:24:48 +0300

    wip

--- resources/js/components/HistoryCollection/HistoryCollection.js
+++ resources/js/components/HistoryCollection/HistoryCollection.js
@@ -8,17 +8,17 @@ const HistoryCollection = ({ id }) => {
 	const { items, initial, error, requested } = useRequestedCollection(`/api/v1/history_for_user_id/${id}`, 100, id);
 
 	const placeholder =  error ? error : ( requested ? 'К сожалению не найдена ваша история книг' : 'Для отображения вашей читательской истории пожалуйста укажите ID читателя' );
-	const title = 'Прочитанное вами';
+	const title = 'История книг пользователя';
 
-	return ( 
+	return (
 		<>
 			<WidgetCollection
 				title={title}
 				items={items}
 				child='Book'
-				placeholder={initial ? <span className='placeholder'>{ placeholder }</span> : <Loader />} 
+				placeholder={initial ? <span className='placeholder'>{ placeholder }</span> : <Loader />}
 			/>
-		</>	
+		</>
 	);
 };
 
