9ff41d5c347855436239cd62ba338e521b58c82f
volkv <pvolkv@gmail.com>
2021-10-24 16:08:46 +0300

    wip

--- app/Helpers/BookHelperService.php
+++ app/Helpers/BookHelperService.php
@@ -8,7 +8,6 @@ use DB;
 class BookHelperService
 {
 
-
     const NEIGHBOUR_MIN_BOOK_COUNT = 5;
     const MAX_NEIGHBOURS_COUNT = 30;
 
@@ -91,44 +90,5 @@ limit 10");
         return $bookData;
     }
 
-    static function cleanTitle(string $title)
-    {
 
-
-        $title = mb_strtolower($title);
-
-        $title = str_replace("рассказы", "", $title);
-        $title = str_replace("рассказ", "", $title);
-        $title = str_replace("фантастич повесть", "", $title);
-        $title = str_replace("повесть", "", $title);
-        $title = str_replace("повести", "", $title);
-        $title = str_replace("книга для чтения на англ яз", "", $title);
-        $title = str_replace("романы", "", $title);
-        $title = str_replace("роман", "", $title);
-        $title = str_replace("перевод с английского", "", $title);
-        $title = str_replace("пер. с англ.", "", $title);
-        $title = str_replace("графический роман", "", $title);
-        $title = str_replace("комикс", "", $title);
-        $title = str_replace("сборник", "", $title);
-        $title = str_replace("сказкапритча", "", $title);
-        $title = str_replace("сказка", "", $title);
-        $title = str_replace("сказки", "", $title);
-        $title = str_replace("пер с фр", "", $title);
-        $title = str_replace("-", "", $title);
-        $title = str_replace("аудиокнига", "", $title);
-        $title = str_replace("фантастические", "", $title);
-        $title = str_replace("для среднего школьного возраста", "", $title);
-        $title = str_replace("для старшего школьного возраста", "", $title);
-        $title = str_replace("перевод с французского", "", $title);
-
-        $title = preg_replace("/\([^)]+\)/", "", $title);
-
-        $title = preg_replace('~[^\p{Cyrillic}a-z0-9_\s-]+~ui', '', $title);
-        $title = preg_replace('/\s\s+/', ' ', $title);
-
-        $title = trim($title, " ");
-
-
-        return $title;
-    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Helpers\BookHelperService;
 use App\Models\Author;
 use App\Models\Book;
 use App\Models\BookUnique;
@@ -22,34 +21,29 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-        // $this->insertBooksIDSFromCirculations();
+        $this->insertBooksIDSFromCirculations();
 
-        //  self::$existingBookIDSCache = Book::pluck('id', 'id')->toArray();
-        //  self::$uniqueBooksCache = BookUnique::pluck('id', 'unique_title')->toArray();
+        self::$existingBookIDSCache = Book::pluck('id', 'id')->toArray();
+        self::$uniqueBooksCache = BookUnique::pluck('id', 'unique_title')->toArray();
 
-        //  self::$rubricsCache = Rubric::pluck('id', 'name')->toArray();
-        //  self::$authorsCache = Author::pluck('id', 'simple_name')->toArray();
+        self::$rubricsCache = Rubric::pluck('id', 'name')->toArray();
+        self::$authorsCache = Author::pluck('id', 'simple_name')->toArray();
 
-        //   $this->insertBooks();
+        $this->insertBooks();
 
-        //  $this->insertAllBooks();
+        $this->insertAllBooks();
 
-        // self::$existingBookIDSCache = Book::pluck('book_unique_id', 'id')->toArray();
-        //  $this->insertCirculations();
+        $this->insertCirculations();
 
-        //  self::$existingBookIDSCache = Book::pluck('id', 'id')->toArray();
-
-        // $this->syncNonExistingBooksWithAPI();
+        $this->syncNonExistingBooksWithAPI();
 
         $this->insertControlCirculations();
-
 
     }
 
 
     public function syncNonExistingBooksWithAPI()
     {
-
 
         $booksWithNoUnique = Book::pluck('book_unique_id', 'id')->toArray();
 
@@ -65,7 +59,6 @@ class DatabaseSeeder extends Seeder
 
             $this->syncWithAPI($bookID);
         }
-
 
     }
 
@@ -203,11 +196,11 @@ class DatabaseSeeder extends Seeder
             $authorID = null;
         } else {
 
-            $authorSimpleName = explode(' ', BookHelperService::cleanTitle($book['author_fullName']))[0];
+            $authorSimpleName = explode(' ', DatabaseSeeder::cleanTitle($book['author_fullName']))[0];
 
             if (!$authorID = (self::$authorsCache[$authorSimpleName] ?? null)) {
 
-                $authorSimpleName = explode(' ', BookHelperService::cleanTitle($book['author_fullName']))[0];
+                $authorSimpleName = explode(' ', DatabaseSeeder::cleanTitle($book['author_fullName']))[0];
 
                 $data = [
 
@@ -242,7 +235,7 @@ class DatabaseSeeder extends Seeder
             $titleAttribute = $book['title'];
         }
 
-        $uniqueTitle = BookHelperService::cleanTitle($titleAttribute);
+        $uniqueTitle = DatabaseSeeder::cleanTitle($titleAttribute);
 
         if (!$uniqueTitle) {
             $uniqueTitle = mb_strtolower($book['title_orig']);
@@ -459,6 +452,47 @@ class DatabaseSeeder extends Seeder
 
         return JsonMachine::fromFile(storage_path('datasets_biblioteki/books_full.jsn'));
 
+    }
+
+    static function cleanTitle(string $title)
+    {
+
+
+        $title = mb_strtolower($title);
+
+        $title = str_replace("рассказы", "", $title);
+        $title = str_replace("рассказ", "", $title);
+        $title = str_replace("фантастич повесть", "", $title);
+        $title = str_replace("повесть", "", $title);
+        $title = str_replace("повести", "", $title);
+        $title = str_replace("книга для чтения на англ яз", "", $title);
+        $title = str_replace("романы", "", $title);
+        $title = str_replace("роман", "", $title);
+        $title = str_replace("перевод с английского", "", $title);
+        $title = str_replace("пер. с англ.", "", $title);
+        $title = str_replace("графический роман", "", $title);
+        $title = str_replace("комикс", "", $title);
+        $title = str_replace("сборник", "", $title);
+        $title = str_replace("сказкапритча", "", $title);
+        $title = str_replace("сказка", "", $title);
+        $title = str_replace("сказки", "", $title);
+        $title = str_replace("пер с фр", "", $title);
+        $title = str_replace("-", "", $title);
+        $title = str_replace("аудиокнига", "", $title);
+        $title = str_replace("фантастические", "", $title);
+        $title = str_replace("для среднего школьного возраста", "", $title);
+        $title = str_replace("для старшего школьного возраста", "", $title);
+        $title = str_replace("перевод с французского", "", $title);
+
+        $title = preg_replace("/\([^)]+\)/", "", $title);
+
+        $title = preg_replace('~[^\p{Cyrillic}a-z0-9_\s-]+~ui', '', $title);
+        $title = preg_replace('/\s\s+/', ' ', $title);
+
+        $title = trim($title, " ");
+
+
+        return $title;
     }
 
 }
