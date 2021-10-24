b70f160c5e82f8fa1329268e8d99bcf19096fa1c
Lumiso <v.v.smolentsev@gmail.com>
2021-10-24 15:36:16 +0300

    wip

--- resources/js/components/MainScreen/index.js
+++ resources/js/components/MainScreen/index.js
@@ -21,6 +21,7 @@ const MainScreen = () => {
 					<input placeholder='ID читателя' onBlur={handleCommit} onKeyDown={handleKeyDown}/>
 				</div>
 			</div>
+			<span className='legend label'>ID читателя в пределах от 1 до 103 - пользователи контрольной группы, остальные пользователи из циркуляций</span>
 			<RecommendedCollection id={id} child='Book' />
 			<NeighboursCollection id={id} child='Person' />
 		</div>
